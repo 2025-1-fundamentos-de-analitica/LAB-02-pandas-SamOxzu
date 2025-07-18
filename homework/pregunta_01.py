38649fd959ccd612c09636094cc89f26108a659d
Oxzu <samgutierrezsa@unal.edu.co>
2025-07-17 19:40:09 -0500

    Solución

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,12 +5,16 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_01():
     """
     ¿Cuál es la cantidad de filas en la tabla `tbl0.tsv`?
-
+    
     Rta/
     40
-
     """
+    df = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    return len(df)
+
+print(pregunta_01())
